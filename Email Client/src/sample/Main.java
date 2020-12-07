83ddf2e459035034147e35c42179683ef97898fe
Raul Palade <raul.palade@edu.unito.it>
2020-12-07 16:58:29 +0100

    Project init

--- Email Client/src/sample/Main.java
+++ Email Client/src/sample/Main.java
@@ -10,14 +10,14 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("leftLayout.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("generalLayout.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 400, 550));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
 
     public static void main(String[] args) {
-
+        launch(args);
     }
 }
