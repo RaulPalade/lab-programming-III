81899d761d36d49730a00d4d34e3a2c84849546f
Raul Palade <raul.palade@edu.unito.it>
2021-03-05 19:20:47 +0100

    Creato layout e progetto per il server

---
+++ Email Server/src/controller/EmailServerController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+/**
+ * @author Raul Palade
+ * @project Email Server
+ * @date 05/03/2021
+ */
+public class EmailServerController {
+    @FXML
+    private Label log;
+}
